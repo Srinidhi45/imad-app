f9a11c9934f8211a22ac8d33274800ba19a3c73e
Srinidhi45 <30649354+Srinidhi45@users.noreply.github.com>
2017-08-05 11:41:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var content = {
+var articleOne = {
     title: 'Article One | Srinidhi',
     heading: 'Article One',
     date: 'Aug 4, 2017',
