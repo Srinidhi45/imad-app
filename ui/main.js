37cd16c27bc65d6a0ea4cad95d528ba034094ed8
Srinidhi45 <30649354+Srinidhi45@users.noreply.github.com>
2017-08-17 11:10:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,7 +39,7 @@ submit.onclick = function () {
     var names = ['name1', 'name2', 'name3'];
     var list = '';
     for(var i=0; i < names.length; i++) {
-         var list = '<li>' + names[i] + '</li>';
+         var list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
