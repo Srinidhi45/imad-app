fba3ff64b89c4b410d480ef986d4d567c9653f8c
Srinidhi45 <30649354+Srinidhi45@users.noreply.github.com>
2017-08-30 16:15:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
+var crypto = require('crypto');
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -79,6 +82,20 @@ function createTemplate(data) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash(input, salt) {
+    // How do we create a hash?
+    var hashed = crypto.pbkdf25ync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+
+app.get('/hash/:input',function (req, res) {
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
+});
+
+
 
 var counter = 0;
 app.get('/counter', function (req, res) {
